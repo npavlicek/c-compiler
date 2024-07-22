be0ef7dd418138b00a4c4c7d5aeb6308e104a92f
Niko Pavlicek <npavlicek@users.noreply.github.com>
2024-07-22 18:19:53 -0400

    lexer char literals

--- README.md
+++ README.md
@@ -9,6 +9,19 @@ I hope to be able to bootstrap this compiler.
 ## To Do
 
 - [ ] Lexer
+    - [ ] Character Literals
+        - [ ] Fix character escape bug. An escaped character hex or oct literal must have the exact length of a byte. A hex value must have two digits and an oct needs three. Anything shorter throws an error.
+        - [ ] Implement character literal prefixes?
+    - [ ] String literals
+    - [ ] Identifiers
+    - [ ] Integer constants
+    - [ ] Floating constants
 - [ ] Recursive Descent Parser
+
+## Missing features
+
+- Character Literals
+    - Multicharacter literals are unsupported.
+    - Character literal prefixes such as `u'\xFF'` are not supported currently. All char literals are considered signed.
 
 Functions such as printf will be built in for debugging purposes and also linking with the actual c standard library would be impossible unless I worked on this for months straight.
--- lexer.c
+++ lexer.c
@@ -63,6 +63,21 @@ int cb_next(CharBuffer *cb)
 	return 1;
 }
 
+int cb_back(CharBuffer *cb)
+{
+	if (cb->eob)
+		return 0;
+
+	if (cb->_cur_idx > 0)
+	{
+		cb->_cur_idx--;
+		cb->cur_char = cb->_buf[cb->_cur_idx];
+		cb->next_char = cb->_buf[cb->_cur_idx + 1];
+	}
+
+	return 1;
+}
+
 // Returns -1 if theres no match, otherwise the token
 int is_keyword(const char *const str)
 {
@@ -77,10 +92,13 @@ int is_keyword(const char *const str)
 }
 
 // return -1 if no match
-int is_punctuator(const CharBuffer *cb)
+int is_punctuator(CharBuffer *cb)
 {
 	char f = cb->cur_char;
 	char s = cb->next_char;
+
+	// In order to process the double char only once when we return to the tokenize function
+	cb_next(cb);
 
 	// double char tokens
 	if (f == '-' && s == '>')
@@ -106,6 +124,8 @@ int is_punctuator(const CharBuffer *cb)
 	if (f == '|' && s == '|')
 		return TOK_OR;
 
+	cb_back(cb);
+
 	// single char tokens
 	for (int i = 0; i < sizeof(single_punctuator_char) / sizeof(char); i++)
 	{
@@ -118,13 +138,157 @@ int is_punctuator(const CharBuffer *cb)
 	return -1;
 }
 
-int tokenize(CharBuffer *cb)
+typedef struct TokenData
 {
+	int _overflow;
+
+	int _buf_max_size;
+	int _str_max_size;
+
+	int _tok_idx;
+	int _ident_idx;
+	int _str_lit_idx;
+	int _int_const_idx;
+	int _float_const_idx;
+
+	int *tokens;
+
+	char **identifiers;
+	char **string_literals;
+
+	int *integer_constants;
+	float *floating_constants;
+} TokenData;
+
+TokenData *alloc_token_data(int buf_max_size, int str_max_size)
+{
+	TokenData *res = calloc(1, sizeof(TokenData));
+	res->_buf_max_size = buf_max_size;
+	res->_str_max_size = str_max_size;
+	res->tokens = calloc(buf_max_size, sizeof(int));
+	res->identifiers = calloc(buf_max_size, sizeof(char *));
+	res->string_literals = calloc(buf_max_size, sizeof(char *));
+	res->integer_constants = calloc(buf_max_size, sizeof(int));
+	res->floating_constants = calloc(buf_max_size, sizeof(float));
+	for (int i = 0; i < buf_max_size; i++)
+	{
+		res->identifiers[i] = calloc(str_max_size, sizeof(char));
+		res->string_literals[i] = calloc(str_max_size, sizeof(char));
+	}
+	return res;
+}
+
+void free_token_data(TokenData *td)
+{
+	for (int i = 0; i < td->_buf_max_size; i++)
+	{
+		free(td->identifiers[i]);
+		free(td->string_literals[i]);
+	}
+	free(td->tokens);
+	free(td->identifiers);
+	free(td->string_literals);
+	free(td->integer_constants);
+	free(td->floating_constants);
+	free(td);
+}
+
+char get_escaped_char(char chr)
+{
+	switch (chr)
+	{
+	case 'a':
+		return '\a';
+	case 'b':
+		return '\b';
+	case 'e':
+		return '\e';
+	case 'f':
+		return '\f';
+	case 'n':
+		return '\n';
+	case 'r':
+		return '\r';
+	case 't':
+		return '\t';
+	case 'v':
+		return '\v';
+	case '\\':
+		return '\\';
+	case '\'':
+		return '\'';
+	case '\"':
+		return '\"';
+	case '\?':
+		return '\?';
+	default:
+		return 0;
+	}
+}
+
+void emit_token(TokenData *buf, int tok)
+{
+	if (buf->_tok_idx >= buf->_buf_max_size)
+	{
+		buf->_overflow = 1;
+		return;
+	}
+
+	buf->tokens[buf->_tok_idx] = tok;
+	buf->_tok_idx++;
+}
+
+void emit_char_literal(TokenData *buf, unsigned char chr)
+{
+	emit_token(buf, TOK_CONSTANT_CHAR);
+	if (isgraph(chr))
+	{
+		printf("\n\nEmitting: %c\n\n", chr);
+	}
+	else
+	{
+		printf("\n\nEmitting: %d\n\n", chr);
+	}
+	emit_token(buf, chr);
+}
+
+TokenData *tokenize(CharBuffer *cb)
+{
+	TokenData *token_data = alloc_token_data(5000, 250);
+
 	int comment_line_mode = 0;
 	int comment_block_mode = 0;
+	int char_literal_mode = 0;
+	int current_line = 1;
 
 	while (cb_next(cb))
 	{
+		// Make sure we dont overflow the token buffer
+		if (token_data->_overflow)
+		{
+			printf("Error: encountered an overflow in the TokenData struct!");
+			return NULL;
+		}
+
+		// detect if a new line is encountered
+		if (cb->cur_char == '\n' || cb->cur_char == '\r')
+		{
+			current_line++;
+		}
+
+		// check to exit char literal mode
+		if (char_literal_mode)
+		{
+			if (cb->cur_char != '\'')
+			{
+				printf("Error: char literals cannot be longer than one character. Line: %d\n", current_line);
+				return NULL;
+			}
+			char_literal_mode = 0;
+			continue;
+		}
+
+		// Check if we are exiting comment modes
 		if (comment_line_mode)
 		{
 			if (cb->cur_char == '\n')
@@ -147,6 +311,109 @@ int tokenize(CharBuffer *cb)
 		if (isspace(cb->cur_char))
 			continue;
 
+		// TODO: ALLOW SHORTER OCT VALUES such as 33 instead of 330
+		// TODO: implement char literal prefixes
+
+		// char literal mode
+		if (cb->cur_char == '\'')
+		{
+			char_literal_mode = 1;
+			// check if the char is an escape character
+			if (cb->next_char == '\\')
+			{
+				cb_next(cb);
+				if (cb->next_char == 'x')
+				{
+					unsigned char char_digits[2];
+					int hex_digits[2];
+
+					cb_next(cb);
+					char_digits[0] = tolower(cb->next_char);
+					cb_next(cb);
+					char_digits[1] = tolower(cb->next_char);
+
+					if (isalpha(char_digits[0]))
+						hex_digits[0] = char_digits[0] - 'a' + 10;
+					else
+						hex_digits[0] = char_digits[0] - '0';
+
+					if (isalpha(char_digits[1]))
+						hex_digits[1] = char_digits[1] - 'a' + 10;
+					else
+						hex_digits[1] = char_digits[1] - '0';
+
+					// check each digit
+					for (int i = 0; i < 2; i++)
+					{
+						if (hex_digits[i] > 15 || hex_digits[i] < 0)
+						{
+							printf("Error: invalid hex escape sequence. Line: %d\n", current_line);
+							return NULL;
+						}
+					}
+
+					int final_val = hex_digits[0] * 16 + hex_digits[1];
+
+					if (final_val > 255 || final_val < 0)
+					{
+						printf("Error: hex escape sequence out of range. Line: %d\n", current_line);
+						return NULL;
+					}
+
+					emit_char_literal(token_data, final_val);
+				}
+				else if (isdigit(cb->next_char))
+				{
+					int oct_digits[3];
+
+					oct_digits[0] = cb->next_char - '0';
+
+					cb_next(cb);
+					oct_digits[1] = cb->next_char - '0';
+
+					cb_next(cb);
+					oct_digits[2] = cb->next_char - '0';
+
+					for (int i = 0; i < 3; i++)
+					{
+						if (oct_digits[i] > 7 || oct_digits[i] < 0)
+						{
+							printf("Error: invalid oct escape sequence. Line: %d\n", current_line);
+							return NULL;
+						}
+					}
+
+					int final_val = oct_digits[0] * 64 + oct_digits[1] * 8 + oct_digits[2];
+
+					if (final_val > 255 || final_val < 0)
+					{
+						printf("Error: oct escape sequence out of range. Line: %d\n", current_line);
+						return NULL;
+					}
+
+					emit_char_literal(token_data, final_val);
+				}
+				else
+				{
+					char escaped_char = get_escaped_char(cb->next_char);
+					if (!escaped_char)
+					{
+						printf("Error: not a valid escape character. Line: %d\n", current_line);
+						return NULL;
+					}
+
+					emit_char_literal(token_data, escaped_char);
+				}
+			}
+			else
+			{
+				emit_char_literal(token_data, cb->next_char);
+			}
+			// Shift our buffer over by an extra one
+			cb_next(cb);
+			continue;
+		}
+
 		// parse out comments
 		if (cb->cur_char == '/' && cb->next_char == '/')
 		{
@@ -154,6 +421,7 @@ int tokenize(CharBuffer *cb)
 			continue;
 		}
 
+		// block comments
 		if (cb->cur_char == '/' && cb->next_char == '*')
 		{
 			comment_block_mode = 1;
@@ -162,10 +430,18 @@ int tokenize(CharBuffer *cb)
 			continue;
 		}
 
+		// punctuators
+		int pctr = is_punctuator(cb);
+		if (pctr != -1)
+		{
+			emit_token(token_data, pctr);
+			continue;
+		}
+
 		printf("%c", cb->cur_char);
 	}
 
-	return 0;
+	return token_data;
 }
 
 int main(int argc, char *argv[])
@@ -183,7 +459,13 @@ int main(int argc, char *argv[])
 	cb->_size = fread(cb->_buf, sizeof(char), cb->_max_size - 1, source_file);
 	fclose(source_file);
 
-	tokenize(cb);
+	TokenData *tok_data = tokenize(cb);
+	if (!tok_data)
+	{
+		return -1;
+	}
+
+	free_token_data(tok_data);
 
 	delete_char_buffer(cb);
 
